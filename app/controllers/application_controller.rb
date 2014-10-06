687e2b0d9c5a40dbdac465d204a0e7210a17f10f
andrea longhi <andrea@spaghetticode.it>
2014-10-06 17:57:25 +0200

    add actions and routes to inspect rails sourcecode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
+require 'coderay'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+    render :text => %{<pre>#{caller_to_links}</pre>}
+  end
+
+  def sourcecode
+    html = CodeRay.scan(code, :ruby).div(line_numbers: :table)
+    render :text => "<pre>#{html}</pre>"
+  end
+
+  private
+
+  def caller_to_links
+    caller.map do |line|
+      %{<a href="#{line}">#{line}</a>}
+    end.join "\n"
+  end
+
+  def source_filename
+    basename = params[:path].split(':')[0]
+    "/#{basename}.rb"
+  end
+
+  def code
+    File.read source_filename
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'application#index'
+  match '*path', to: 'application#sourcecode', via: :get
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
